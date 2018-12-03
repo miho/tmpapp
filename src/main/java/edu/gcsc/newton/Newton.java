2745950ead769bba797f63da858afdcd428d193d
Michael Hoffer <info@michaelhoffer.de>
2018-12-03 16:14:29 +0100

    Additional tests added and functionality implemented - relates to issue #5 and #6

--- src/main/java/edu/gcsc/newton/Newton.java
+++ src/main/java/edu/gcsc/newton/Newton.java
@@ -1,13 +1,22 @@
 package edu.gcsc.newton;
 
 public class Newton {
-    public double findRoot(Function f, Function df, double x, double tol) {
+    public double findRoot(Function f, Function df, double x, int maxIter, double tol) {
         
         double h = tol + 1;
+        int iter = 0;
 
-        while(Math.abs(h)> tol) {
+        while(Math.abs(h) > tol) {
             h = f.eval(x)/df.eval(x);
             x -= h;
+
+            iter++;
+
+            if(iter > maxIter) {
+                throw new RuntimeException("Does not converge after max number of iterations. x = " + x);
+            } else if(Double.isNaN(x)) {
+                throw new RuntimeException("Does not converge. Cannot find root, x=NaN");
+            }
         }
 
         return x;
--- src/test/java/edu/gcsc/newton/NewtonTest.java
+++ src/test/java/edu/gcsc/newton/NewtonTest.java
@@ -7,7 +7,7 @@ import edu.gcsc.newton.Newton.Function;
 
 public class NewtonTest {
 
-    @Test public void testFindRoot() {
+    @Test(timeout=1000) public void testFindRoot() {
         
         double tol = 1e-12;
 
@@ -26,14 +26,57 @@ public class NewtonTest {
         expected = -1;
 
         createFindRootTest(f, df, x0, expected, tol);
-        
+
+        try  {
+            f  = x -> 1;
+            df = x -> 0;
+            x0 = 0;
+            expected = 0;
+
+            createFindRootTest(f, df, x0, expected, tol);
+
+            fail();
+        } catch(Exception ex) {
+            // TODO check exception type etc.
+        }
+
+        // NON LINEAR CASES
+
+        f  = x -> x*x;
+        df = x -> 2*x;
+        x0 = 1;
+        expected = 0;
+
+        createFindRootTest(f, df, x0, expected, tol);
+
+        f  = x -> Math.sin(x);
+        df = x -> Math.cos(x);
+        x0 = 1;
+        expected = 0;
+
+        createFindRootTest(f, df, x0, expected, tol);
+
+        f  = x -> Math.sin(x);
+        df = x -> Math.cos(x);
+        x0 = 2.5;
+        expected = Math.PI;
+
+        createFindRootTest(f, df, x0, expected, tol);
+
+        f  = x -> Math.sin(x);
+        df = x -> Math.cos(x);
+        x0 = 5.5;
+        expected = 2*Math.PI;
+
+        createFindRootTest(f, df, x0, expected, tol);
+
     }
 
     private void createFindRootTest(Function f, Function df, double x0, double expected, double tol) {
         
         Newton newton = new Newton();
 
-        assertEquals(expected, newton.findRoot(f,df,x0,tol), tol);
+        assertEquals(expected, newton.findRoot(f, df, x0, 100, tol), tol);
 
     }
 }
